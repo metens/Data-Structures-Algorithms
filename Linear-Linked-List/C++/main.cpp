1526832ae269585ced2ad1df1ebd2b26fdc0f052
metens <nathanmetens1@gmail.com>
2026-02-03 07:55:15 -0800

    Creating a node and printing the data

--- Linear-Linked-List/C++/main.cpp
+++ Linear-Linked-List/C++/main.cpp
@@ -1,9 +1,19 @@
 #include <iostream>
 using namespace std;
 
+struct node {
+    int data;
+    node* next;
+};
+
 int main() {
 
-    cout << "Hello" << endl;
+    node * head = new node;
+
+    head -> data = 10;
+    head -> next = nullptr;
+
+    cout << "head -> data = " << head -> data << endl;
 
     return 0;
 }
